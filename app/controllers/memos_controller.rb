fa869a6ca98b1bd94b8377f579d66b2c103bae26
Taichi Watabe <taichi722neville@gmail.com>
2020-01-01 19:35:14 +0000

    initial commit

---
+++ app/controllers/memos_controller.rb
@@ -0,0 +1,38 @@
+class MemosController < ApplicationController
+    def index
+        # トップページ
+        @memo = Memo.all
+    end
+    
+    def new
+        # New Memoがクリックされた時に新しいレコードを追加するアクション
+        
+    end
+    
+    def create
+        # 新規作成ページにて送信ボタンが選択された時にレコードの追加をするアクション
+        Memo.create(title:params["memos"]["title"],body:params["memos"]["body"])
+        redirect_to "/"
+    end
+    
+    def edit
+        # メモの編集ページにアクセスするアクション
+        @memo = Memo.find(params["id"])
+    end
+    
+    def update
+        # メモの編集を行うアクション
+        memo = Memo.find(params["id"])
+        memo.title = params["memos"]["title"]
+        memo.body = params["memos"]["body"]
+        memo.save
+        redirect_to "/"
+    end
+    
+    def delete
+        # メモの削除を行うアクション
+        memo = Memo.find(params["id"])
+        memo.destroy
+        redirect_to "/"
+    end
+end
